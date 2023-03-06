085c21f1bb12833fcbf3bf5038e648fddea6fcb7
A$H <91582711+Ashwani2529@users.noreply.github.com>
2023-03-06 13:12:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ from PIL import Image
 from gtts import gTTS
 import os
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\91961\Downloads\TEI2S-master\terr\tesseract.exe'
-src_path = './'
+directory_path = 'C:/Users/91961/Downloads/TEI2S-master/'
+for filename in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, filename)
+        with Image.open(file_path) as img:
+                s=filename
+                break
 def get_string(img_path):
-    result = pytesseract.image_to_string(Image.open(src_path + "3.png"))
-    return result
-img2txt = get_string(src_path + "3.png")
+        result = pytesseract.image_to_string(Image.open(str(s)))
+        return result
+img2txt = get_string(str(s))
 print('--- Recognizing text from image ---'+'\n'+img2txt)
 myobj = gTTS(text=img2txt, lang='en', slow=False)
 myobj.save('speech.mp3')
 os.system('speech.mp3')
+os.remove('speech.mp3')
