ebb2d107b93acbe3ce63c4401dd1be9ea5455dac
A$H <91582711+Ashwani2529@users.noreply.github.com>
2023-03-09 13:47:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import pytesseract
 from PIL import Image
 from gtts import gTTS
 import os
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\91961\Downloads\TEI2S-master\terr\tesseract.exe'
-directory_path = 'C:/Users/91961/Downloads/TEI2S-master/'
+import subprocess
+subprocess.Popen('start microsoft.windows.camera:', shell=True)
+input()
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\91961\Downloads\Image-To-Speech\terr\tesseract.exe'
+directory_path = 'C:/Users/91961/Downloads/Image-To-Speech/'
 for filename in os.listdir(directory_path):
         file_path = os.path.join(directory_path, filename)
-        with Image.open(file_path) as img:
-                s=filename
-                break
+        try:
+                with Image.open(file_path) as img:
+                        if (img.format== 'JPEG') or (img.format=='PNG'):
+                                s=filename
+                                break
+        except:
+                pass
 def get_string(img_path):
         result = pytesseract.image_to_string(Image.open(str(s)))
         return result
